8f175faa7b7191dd0a5dc86c433fd4614ad979de
Doug Schneider <reioni90@gmail.com>
2011-06-30 20:19:38 -0600

    changed some of the directory structure, created compile.c which will take all of the files in src and compile them into the final .js file

---
+++ compile.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILES "files.txt"
+#define FIND_COMMAND "find src -type f > files.txt"
+#define MAX_SIZE 500
+#define OUTPUT_FILE "main.js"
+
+void processFile(char * fileName);
+void killAll(char * error);
+
+FILE * input = NULL;
+FILE * output = NULL;
+FILE * files = NULL;
+
+/*
+ * This small program copies all of the source from files inside of the
+ * src folder and puts them into one file that can then be loaded into
+ * the html file to run SoBr.
+ */
+int main()
+{
+	extern FILE * output;
+	extern FILE * files;
+
+	char fileName[MAX_SIZE];
+
+	system(FIND_COMMAND);
+	files = fopen(FILES, "r");
+	if(files == NULL)
+		killAll("Error opening the list of files");
+	output = fopen(OUTPUT_FILE, "w");
+	if(output == NULL)
+		killAll("Error creating the output file");
+	// loop through all of the js files
+	while(fgets(fileName, MAX_SIZE, files) != NULL)
+	{
+		// remove the '\n' from the fileName
+		int i = strlen(fileName);
+		fileName[i - 1] = '\0';
+
+		processFile(fileName);
+	}
+
+	// close files
+	fclose(files);
+	fclose(output);
+
+	printf("Done processing the files, output should now be in %s\n", OUTPUT_FILE);
+
+	return 0;
+}
+
+/*
+ *	Open the specified file and copy all of it's contents
+ *	over to the output file. The file is given a header with
+ *	the input files name.
+ */
+void processFile(char * fileName)
+{
+	extern FILE * input;
+	extern FILE * output;
+
+	char line[MAX_SIZE];
+
+	input = fopen(fileName, "r");
+	if(input == NULL)
+		killAll("Error opening input file");
+	fprintf(output, "//---------------- starting %s -----------------------//\n", fileName);
+	while(fgets(line, MAX_SIZE, input) != NULL)
+		fprintf(output, "%s", line);
+
+	fclose(input);
+}
+
+/*
+ *	Close any file pointers, print the specified error message
+ *	and exit the program.
+ */
+void killAll(char * error)
+{
+	extern FILE * input;
+	extern FILE * output;
+	extern FILE * files;
+	
+	if(input != NULL)
+		fclose(input);
+	if(output != NULL)
+		fclose(output);
+	if(files != NULL)
+		fclose(files);
+
+	printf("Error: %s\nDying now!\n", error);
+	exit(-1);
+}
